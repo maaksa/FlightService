85dda2f1540a21f1e438036e37172e0ae1c936ff
maaksa <milosmilos@ymail.com>
2020-12-11 17:15:26 +0100

    Initial commit

---
+++ src/test/java/sk_microservices/FlightService/FlightServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sk_microservices.FlightService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
