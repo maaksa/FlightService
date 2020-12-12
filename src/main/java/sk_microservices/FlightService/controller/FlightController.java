3f2a17273955901ddeb0ff132e0e47525714e89a
maaksa <milosmilos@ymail.com>
2020-12-12 01:04:29 +0100

    implementirano dodavanje letova

--- src/main/java/sk_microservices/FlightService/controller/FlightController.java
+++ src/main/java/sk_microservices/FlightService/controller/FlightController.java
@@ -1,12 +1,47 @@
 package sk_microservices.FlightService.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sk_microservices.FlightService.entites.Airplane;
+import sk_microservices.FlightService.entites.Flight;
+import sk_microservices.FlightService.forms.AddFlightForm;
+import sk_microservices.FlightService.repository.AirplaneRepository;
+import sk_microservices.FlightService.repository.FlightRepository;
 
 @RestController
-@RequestMapping("flight")
+@RequestMapping("/flight")
 public class FlightController {
 
+    private AirplaneRepository airplaneRepository;
+    private FlightRepository flightRepository;
+
+    @Autowired
+    public FlightController(AirplaneRepository airplaneRepository, FlightRepository flightRepository) {
+        this.airplaneRepository = airplaneRepository;
+        this.flightRepository = flightRepository;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addFlight(@RequestBody AddFlightForm addFlightForm) {
+
+        try {
+            Airplane avion = addFlightForm.getAvion();
+            airplaneRepository.save(avion);
+
+            Flight let = new Flight(avion, addFlightForm.getPocetnaDestinacija(), addFlightForm.getKrajnjaDestinacija(),
+                    addFlightForm.getDuzinaLeta(), addFlightForm.getCena());
+            System.out.println(let);
+
+            flightRepository.save(let);
+
+            return new ResponseEntity<String>("successfully added", HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 
 
 }
